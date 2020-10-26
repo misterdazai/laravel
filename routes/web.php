398c078914005cfa5b4daf20e16a68d51d2866cf
Nik <nik25611@gmail.com>
2020-10-26 14:32:49 +0300

    123

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/sign_up', function () {
 Route::post('/sign_up/submit', 'App\Http\Controllers\SignUpController@submit')->name('sign_up');
 
 Route::post('/sign_in/submit', 'App\Http\Controllers\SignInController@submit')->name('sign_in');
+
+Route::resource('users', 'App\Http\Controllers\UserController');
